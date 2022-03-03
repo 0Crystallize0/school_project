430f5ceb00714930612cf8c172d8e846653ec7b6
Vadim D. Leukhin <0Crystallize@gmail.com>
2022-03-04 02:51:05 +0300

    Added USSR history part Development testing part

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
-let myButton = document.getElementById("btn-back-to-top");
+const myButton = document.getElementById("btn-back-to-top");
 
 window.onscroll = function () {
-    scrollFunction();
-};
-
-function scrollFunction() {
-    if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-    ) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         myButton.style.display = "block";
     } else {
         myButton.style.display = "none";
     }
-}
+};
 
-myButton.addEventListener("click", backToTop);
-
-function backToTop() {
+myButton.addEventListener("click", () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
+});
+
+const testBtn = document.querySelector('.test-start');
+const exit = document.querySelector('.exit-btn');
+const test = document.querySelector('.test');
+
+testBtn.addEventListener('click', () => {
+    test.classList.add('open');
+});
+
+exit.addEventListener('click', () => {
+    test.classList.remove('open')
+})
