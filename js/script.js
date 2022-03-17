294c417862c962c58ba9db2f264a49309bbf8cd4
Vadim D. Leukhin <0Crystallize@gmail.com>
2022-03-17 22:10:18 +0300

    test OOP rework

--- js/script.js
+++ js/script.js
@@ -17,67 +17,107 @@ myBtn.addEventListener("click", () => {
 
 // Test
 
-const testBtn = document.querySelector('.test-start');
-const test = document.querySelector('.test');
-let answers = [];
+const question = {
+    test: document.querySelector('.test'),
+    questions: document.querySelectorAll('.question'),
+    currentQuestion: document.querySelector('.current-question'),
+    index: -1,
+    answers: [],
 
-testBtn.addEventListener('click', () => {
-    test.classList.add('open');
-    questions();
+    updateCurrentQuestion() {
+        this.index++;
+        this.currentQuestion.classList.remove('current-question');
+        this.questions[this.index].classList.add('current-question');
+        this.currentQuestion = document.querySelector('.current-question');
+    },
+
+    startTest() {
+        this.test.classList.add('open');
+
+        this.updateCurrentQuestion();
+
+        document.querySelectorAll('.test-next')
+            .forEach(target => target.addEventListener('click', this.nextQuestion));
+
+        document.querySelectorAll('.exit-btn')
+            .forEach(target => target.addEventListener('click', this.closeTest));
+    },
+
+    closeTest() {
+        if (confirm('Завершить тест?')) {
+            question.test.classList.remove('open');
+            question.index = -1;
+            question.currentQuestion.classList.remove('current-question');
+            question.questions[0].classList.add('current-question');
+            question.resetAnimation();
+
+            document.querySelectorAll('.test-next')
+                .forEach(target => target.removeEventListener('click', this.nextQuestion));
+
+            document.querySelectorAll('.exit-btn')
+                .forEach(target => target.removeEventListener('click', this.closeTest));
+        }
+    },
+
+    finishTest() {
+        this.answers = this.answers.filter(answer => answer !== '0');
+        document.querySelector('.final-score').innerHTML = 'Правильно: ' + this.answers.length + ' / 15';
+    },
+
+    nextQuestion() {
+        question.writeAnswer();
+        question.updateCurrentQuestion();
+        question.resetQuestion();
+        question.animate();
+    },
+
+    writeAnswer() {
+        let answer = this.currentQuestion.querySelector('input[name="answer"]:checked').value;
+        this.answers.push(answer);
+    },
+
+    resetQuestion() {
+        this.testHeading = this.currentQuestion.querySelector('.test-heading');
+        this.testQuestion = this.currentQuestion.querySelector('.test-question');
+        this.testAnswer = this.currentQuestion.querySelector('.test-answer');
+    },
+
+    animate() {
+        setTimeout(() => {
+            this.testHeading.classList.add('test-heading-animation');
+            this.testQuestion.classList.add('test-question-animation');
+            this.testAnswer.classList.add('test-answer-animation');
+        }, 700);
+    },
+
+    resetAnimation() {
+        this.testQuestion.classList.remove('test-question-animation');
+        this.testAnswer.classList.remove('test-answer-animation');
+
+        document.querySelectorAll('.test-heading')
+            .forEach(target => target.classList.remove('test-heading-animation'));
+
+        document.querySelectorAll('.test-question')
+            .forEach(target => target.classList.remove('test-question-animation'));
+
+        document.querySelectorAll('.test-question')
+            .forEach(target => target.classList.remove('test-answer-animation'));
+
+        document.querySelectorAll('.form-check-input')
+            .forEach(target => target.checked = false);
+    }
+}
+
+document.querySelector('.test-start').addEventListener('click', () => {
+    question.startTest();
+    question.resetQuestion();
+    question.animate();
 });
 
-const animation = (testHeading, testQuestion, testAnswer) => {
-    testHeading.classList.add('test-heading-animation');
-    testQuestion.classList.add('test-question-animation');
-    testAnswer.classList.add('test-answer-animation');
-}
+document.querySelector('.test-score').addEventListener('click', () => {
+    question.finishTest();
+});
 
-const close = () => {
-    test.classList.remove('open');
-}
-
-const end = () => {
-    document.querySelector('.current-question').classList.remove('current-question');
-
-    answers = answers.filter(answer => answer !== '0');
-    console.log(answers.length);
-    document.querySelector('.score').style.display = "block";
-    document.querySelector('.final-score').innerHTML = "Правильно: " + answers.length + " / 15";
-}
-
-const questions = () => {
-    const questions = document.querySelectorAll('.question')
-    let currentQuestion = document.querySelector('.current-question');
-    let index = 0;
-
-    let testHeading = currentQuestion.querySelector('.test-heading');
-    let testQuestion = currentQuestion.querySelector('.test-question');
-    let testAnswer = currentQuestion.querySelector('.test-answer');
-
-    setTimeout(animation, 800, testHeading, testQuestion, testAnswer);
-
-    const nextBtn = () => {
-        let answer = currentQuestion.querySelector('input[name="answer"]:checked').value;
-        answers.push(answer);
-
-        currentQuestion.classList.remove('current-question');
-
-        index++;
-        questions[index].classList.add('current-question');
-
-        currentQuestion = document.querySelector('.current-question');
-        testHeading = currentQuestion.querySelector('.test-heading');
-        testQuestion = currentQuestion.querySelector('.test-question');
-        testAnswer = currentQuestion.querySelector('.test-answer');
-
-        setTimeout(animation, 800, testHeading, testQuestion, testAnswer);
-    }
-
-    document.querySelectorAll('.test-next')
-        .forEach(target => target.addEventListener('click', nextBtn));
-
-    document.querySelectorAll('.exit-btn')
-        .forEach(target => target.addEventListener('click', close));
-
-    document.querySelector('.test-end').addEventListener('click', end);
-}
+document.querySelector('.test-end').addEventListener('click', () => {
+    question.closeTest();
+});
